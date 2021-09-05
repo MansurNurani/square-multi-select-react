c0d67c6c092549978eacf0e6a4fa5e90a71ecaec
MansurNurani <mansur.nurani@gmail.com>
2021-09-05 16:08:51 +0430

    first commit

---
+++ src/Compoents/todo.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import "./file.css"
+export default class Todo extends Component {
+    state = { selectedIds: [] }
+    arrayRemove=(arr, value) => arr.filter(a=>a !== value);
+    handleClick = (e) => {
+        let id = parseInt(e.target.dataset.id);
+        if (this.state.selectedIds.includes(id)) {
+            let res = this.arrayRemove(this.state.selectedIds, id);
+            this.setState({ selectedIds: res });
+        }
+        else {
+            let res = [...this.state.selectedIds, id];
+            this.setState({ selectedIds: res });
+        }
+    }
+    render() {
+        let style1 = { backgroundColor: "green" };
+        console.log("in render: " + this.state.selectedIds);
+        return (
+            <div id="flexContainer1">
+                {[0, 1, 2, 3, 4, 5, 6, 7, 8].map(a => (
+                    <div data-id={a} className="MyClass" style={this.state.selectedIds.includes(a) ? style1 : null} onClick={this.handleClick}></div>
+                ))}
+            </div>
+        )
+    }
+}
